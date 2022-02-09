de658d0b5130bf230b44eef2126bc5f13e60b62f
Sabari Ganesh <k.sabarii.ganesh@gmail.com>
2022-02-09 23:10:18 +0530

    progress

--- src/components/home.js
+++ src/components/home.js
@@ -6,15 +6,21 @@ import loop from "./loop.png";
 import { NavLink } from "react-router-dom";
 import "animate.css/animate.min.css";
 import ScrollAnimation from "react-animate-on-scroll";
+import creator from "../media/me.jpg"
+import insta from "../media/social/insta.png"
+import linkedin from "../media/social/linkedin.png"
+import github from "../media/social/github.png"
+import mail from "../media/social/mail.png"
+import frontend from "../media/frontend.png"
+import resource from "../media/resource.png"
 const Home = () => {
   return (
     <div className="home">
       <div className="intro">
-        <br />
-        <br />
+     
         {/* <ScrollAnimation animateIn="animate__fadeIn" animateOnce={true}>    */}
         <div className="introwords">
-          <span className>EXPLORE COMPUTER SCIENCE</span>
+          <span >EXPLORE <br/> COMPUTER <br/> SCIENCE</span>
         </div>
         {/* </ScrollAnimation> */}
 
@@ -43,12 +49,12 @@ const Home = () => {
             <button className="go">Go Ahead</button>
           </NavLink>
           <a href="#" style={{ textDecoration: "none" }}>
-            <button className="creator">
+            <button className="creatorbutton">
               <span className="text">CREATOR</span>
             </button>
           </a>
           <a href="#" style={{ textDecoration: "none" }}>
-            <button className="creator">
+            <button className="creatorbutton">
               <span className="text">OPEN SOURCE</span>
             </button>
           </a>
@@ -56,10 +62,7 @@ const Home = () => {
       </ScrollAnimation>
 
       <div className="introsubtitle">
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
+  
         No more Fkin excuses , get ur resource and START ROCKING !!
       </div>
       <br />
@@ -79,23 +82,59 @@ const Home = () => {
       <div className="stats">
         <div className="statsbox">
           <div className="statsbox_content">
-              <div>{20} +</div>  
-            <div> Departments</div>
+              <div>{20} + Depts</div>
           </div>
         </div>
         <div className="statsbox">
           <div className="statsbox_content">
-              <div>{1000} +</div>  
-            <div> Modules</div>
+              <div>{1000} + Modules</div>
           </div>
         </div>
         <div className="statsbox">
           <div className="statsbox_content">
-              <div>{200} +</div>  
-            <div> Lectures</div>
+              <div>{200} + Lectures</div>
           </div>
         </div>
       </div>
+    <br/><br/>
+      <div className = "creatortitle">MEET THE CREATOR 😈</div>
+
+    <div className = "creatorcardcontainer">
+<div className = "creatorcard">
+    <img src = {creator} width = "300rem" className = "creatordp"/>
+    <div className = "creatorProfile">
+
+
+
+    <div className = "creatorName">
+      Sabari Ganesh
+    </div>
+    <div className = "CreatorSocials">
+    <img src = {insta}/>
+    <img src = {linkedin}/>
+    <img src = {github}/>
+    <img src = {mail}/>
+    </div>
+    </div>
+</div>
+
+    </div>
+    <div className = "creatortitle">OPEN SOURCE 🚀</div>
+    <div id = "opensource">
+      <div className = "opensourcecontainer">
+      <div className = "resource">
+      <img src = {resource} className = "os_categ_pic" />
+      <div className = "os_categ_title"> RESOURCE </div>
+      </div>
+      <div className = "frontend">
+      <img src = {frontend} className = "os_categ_pic"/>
+      <div className = "os_categ_title">FRONTEND</div>
+      </div>
+ 
+      </div>
+    </div>
+
+
     </div>
   );
 };
